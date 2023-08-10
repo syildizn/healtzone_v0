3436aff2415fd13a9f5d06f77d95462cabaceb0f
Seçkin Yıldız <syildiznbjk@gmail.com>
2023-08-10 22:47:03 +0300

    .

---
+++ lib/screens/models/homePageCategoriesModel.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart'; // flutter_svg kütüphanesi
+import 'package:intl/intl.dart';
+
+class CategoryModel {
+  final String name;
+  final String svgPath; // SVG dosyasının yolu
+  final void Function() onTap;
+
+  CategoryModel({
+    required this.name,
+    required this.svgPath,
+    required this.onTap,
+  });
+}
--- lib/screens/publications.dart
+++ lib/screens/publications.dart
@@ -1,7 +1,14 @@
+import 'package:diacritic/diacritic.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:healtzone_v0/screens/doctor.dart';
 import 'package:healtzone_v0/screens/offers.dart';
+import 'package:intl/intl.dart';
 
+import 'models/homePageCategoriesModel.dart';
+
+//ilan sayfası-ana sayfa
 class PublicationsScreen extends StatefulWidget {
   static String routeName = "PublicationsPage";
   const PublicationsScreen({Key? key}) : super(key: key);
@@ -11,9 +18,42 @@ class PublicationsScreen extends StatefulWidget {
 }
 
 class _PublicationsScreenState extends State<PublicationsScreen> {
-
   int currentPage = 1; //  1: İlanlarım, 2: Doktor Bul, 3: Teklifler
 
+  TextEditingController searchController = TextEditingController();
+
+  List<CategoryModel> categories = [
+    CategoryModel(
+      name: 'Kadın Doğum',
+      svgPath: 'assets/icons/obstetrics.svg', // SVG dosyasının yolu
+      onTap: () {
+        print("tıklandı 1 ");
+      },
+    ),
+    CategoryModel(
+      name: 'Diş Doktoru',
+      svgPath: 'assets/icons/dentist.svg', // SVG dosyasının yolu
+      onTap: () {
+        print("tıklandı 2 ");
+      },
+    ),
+    // Diğer kategorileri burada ekleyebilirsiniz
+  ];
+
+  List<CategoryModel> filteredCategories = [];
+
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    filteredCategories = categories;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,68 +61,140 @@ class _PublicationsScreenState extends State<PublicationsScreen> {
       appBar: AppBar(title: Text("İlanlarım")),
       body: Column(
         children: <Widget>[
+          Padding(//üstteki üç tuş
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: currentPage == 1 ? Colors.blue : Colors.white,
+                        onPrimary:
+                            currentPage == 1 ? Colors.white : Colors.black,
+                      ),
+                      onPressed: () {
+                        // 360 view butonuna tıklandığında yapılacak işlemler
+                        setState(() {
+                          currentPage = 1;
+                        });
+                        Navigator.pushNamed(
+                            context, PublicationsScreen.routeName);
+                      },
+                      child: Text('İlanlarım'),
+                    ),
+                  ),
+                  Container(width: 2, color: Colors.black),
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: currentPage == 2 ? Colors.blue : Colors.white,
+                        onPrimary:
+                            currentPage == 2 ? Colors.white : Colors.black,
+                      ),
+                      onPressed: () {
+                        // Text butonuna tıklandığında yapılacak işlemler
+                        setState(() {
+                          currentPage = 2;
+                        });
+                        Navigator.pushNamed(context, Doctor.routeName);
+                      },
+                      child: Text('Doktor Bul'),
+                    ),
+                  ),
+                  Container(width: 2, color: Colors.black),
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: currentPage == 3 ? Colors.blue : Colors.white,
+                        onPrimary:
+                            currentPage == 3 ? Colors.white : Colors.black,
+                      ),
+                      onPressed: () {
+                        // Smart Screen butonuna tıklandığında yapılacak işlemler
+                        setState(() {
+                          currentPage = 3;
+                        });
+                        Navigator.pushNamed(context, OffersPage.routeName);
+                      },
+                      child: Text('Teklifler'),
+                    ),
+                  ),
+                ],
+              )),
+          // aşağısı kategori arama işlemini yapıyor
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: currentPage == 1 ? Colors.blue : Colors.white,
-                      onPrimary: currentPage == 1 ? Colors.white : Colors.black,
-                    ),
-                    onPressed: () {
-                      // 360 view butonuna tıklandığında yapılacak işlemler
-                      setState(() {
-                        currentPage = 1;
-                      });
-                      Navigator.pushNamed(context,
-                          PublicationsScreen.routeName);
-                    },
-                    child: Text('İlanlarım'),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 7,
+                    offset: Offset(0, 3),
                   ),
+                ],
+              ),
+              child: TextField(
+                keyboardType: TextInputType.text,
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp(r'[a-zA-ZğüışöçİĞÜŞÖÇ]')),
+                ],
+                controller: searchController,
+                decoration: InputDecoration(
+                  hintText: 'Kategori ara...',
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.all(20),
                 ),
-                Container(width: 2, color: Colors.black),
-                Expanded(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: currentPage == 2 ? Colors.blue : Colors.white,
-                      onPrimary: currentPage == 2 ? Colors.white : Colors.black,
+                onChanged: (value) {
+                  setState(() {
+                    filteredCategories = categories
+                        .where((category) =>
+                        category.name.toLowerCase().contains(value.toLowerCase()))
+                        .toList();
+                  });
+                },
+              ),
+            ),
+          ),
+
+
+                // aşağısı kategorileri oluşturan kısım
+          Expanded(flex: 5,
+            child: ListView.builder(
+              itemCount: filteredCategories.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: filteredCategories[index].onTap,
+                  child: Container(
+                    height: 100,
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(30),
                     ),
-                    onPressed: () {
-                      // Text butonuna tıklandığında yapılacak işlemler
-                      setState(() {
-                        currentPage = 2;
-                      });
-                      Navigator.pushNamed(context,
-                          Doctor.routeName);
-                    },
-                    child: Text('Doktor Bul'),
-                  ),
-                ),
-                Container(width: 2, color: Colors.black),
-                Expanded(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: currentPage == 3 ? Colors.blue : Colors.white,
-                      onPrimary: currentPage == 3 ? Colors.white : Colors.black,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SvgPicture.asset(
+                          filteredCategories[index].svgPath,
+                          width: 55,
+                          height: 55,
+                        ),
+                        Text(filteredCategories[index].name,style: TextStyle(fontSize: 22)),
+                      ],
                     ),
-                    onPressed: () {
-                      // Smart Screen butonuna tıklandığında yapılacak işlemler
-                      setState(() {
-                        currentPage = 3;
-                      });
-                      Navigator.pushNamed(context,
-                          OffersPage.routeName);
-                    },
-                    child: Text('Teklifler'),
                   ),
-                ),
-              ],
-            )
+                );
+              },
+            ),
 
           ),
-          Expanded(
+          // sağ alt köşedeki tuş
+          Expanded(flex: 1,
             child: Align(
               alignment: Alignment.bottomRight,
               child: Container(
@@ -93,7 +205,9 @@ class _PublicationsScreenState extends State<PublicationsScreen> {
                     Positioned(
                       bottom: 50,
                       right: 90,
-                      child: Text('İlan Ver',style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold)),
+                      child: Text('İlan Ver',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold)),
                     ),
                     Positioned(
                       bottom: 30,
@@ -112,12 +226,10 @@ class _PublicationsScreenState extends State<PublicationsScreen> {
               ),
             ),
           ),
-
-
+  
           // Diğer widget'lar buraya eklenebilir
         ],
       ),
-
     );
   }
 }
